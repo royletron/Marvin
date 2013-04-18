449f6669866b56b0cb987bb2f66018c85cd7cd52
Darren <darren.royle@oup.com>
2013-04-18 12:13:13 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marvin::Application.config.secret_token = '5ad794e2e29cfdd425046d038fef7a83ca151892096e6c0c90e72512383b3f28d8e7d2f8a306002b0248941920add6c16ff85c1b717a66e287e4e21489eea5ac'
